2f586eb746cb137023b8d28ea349b9fc1136e531
ykob <info@tplh.net>
2022-10-13 21:31:46 +0900

    added the main header element.

--- src/main.tsx
+++ src/main.tsx
@@ -18,6 +18,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <h1>Skyrim Archemy Cheatsheet</h1>
     <RouterProvider router={router} />
   </React.StrictMode>
 )
