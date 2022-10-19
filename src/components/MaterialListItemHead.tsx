5beec951e25f10a70443da092b813f8c7375dd72
ykob <info@tplh.net>
2022-10-19 13:01:34 +0900

    arranged the classnames of MaterialListHead component.

--- src/components/MaterialListItemHead.tsx
+++ src/components/MaterialListItemHead.tsx
@@ -7,8 +7,25 @@ type Props = {
 }
 
 export const MaterialListItemHead = function (props: Props) {
+  const classnames = [
+    'flex',
+    'flex-col',
+    'justify-center',
+    'items-end',
+    'gap-2',
+    'cursor-pointer',
+    'h-20',
+    'px-2',
+    'md:h-12',
+    'lg:flex-row',
+    'lg:justify-end',
+    'lg:items-center',
+    'lg:gap-4',
+    'lg:h-10',
+  ].join(' ')
+
   return (
-    <div className="flex flex-col justify-center items-end gap-2 cursor-pointer h-20 px-2 md:h-12 lg:flex-row lg:justify-end lg:items-center lg:gap-4 lg:h-10">
+    <div className={classnames}>
       <MaterialListItemLabel label={props.label} />
       <CategoryItem categoryId={props.categoryId} />
     </div>
