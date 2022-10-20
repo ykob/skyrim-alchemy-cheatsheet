ec294b63af2e4fa753bdb5c7e308de1a09fed4a2
ykob <info@tplh.net>
2022-10-20 20:38:30 +0900

    update styles of the global header.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { Outlet } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <h1 className="text-2xl">Skyrim Archemy Cheatsheet</h1>
-      <div className="px-4 py-12 lg:px-8">
-        <Outlet />
+    <div className="px-4 pt-8 pb-16 lg:px-8">
+      <div className="flex mb-8">
+        <h1 className="text-2xl">Skyrim Archemy Cheatsheet</h1>
       </div>
+      <Outlet />
     </div>
   )
 }
