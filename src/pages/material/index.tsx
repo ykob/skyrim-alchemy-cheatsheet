9fd9ec86213c8164c3fb9f432ceb0af00917ca38
ykob <info@tplh.net>
2022-11-17 00:12:03 +0900

    added material list on material page.

--- src/pages/material/index.tsx
+++ src/pages/material/index.tsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom'
 import { materials as dataMaterials } from '../../data'
 import { MaterialContent } from '../../components/features/material-content/MaterialContent'
+import { MaterialList } from '../../components/features/material-list/MaterialList'
 
 export const PageMaterial = function () {
   const { id } = useParams()
@@ -9,15 +10,23 @@ export const PageMaterial = function () {
       ? dataMaterials.find((o) => o.id === parseInt(id))
       : undefined
 
-  if (data) {
-    return (
+  if (!data) {
+    return <div>This material is not found.</div>
+  }
+
+  const materials = dataMaterials.filter((o) => {
+    return o.effectIds.filter((p) => data.effectIds.indexOf(p) > -1).length > 0
+  })
+
+  return (
+    <>
       <MaterialContent
+        className="mb-8"
         name={data.name}
         effectIds={data.effectIds}
         category={data.category}
       />
-    )
-  } else {
-    return <div>This material is not found.</div>
-  }
+      <MaterialList materials={materials} />
+    </>
+  )
 }
