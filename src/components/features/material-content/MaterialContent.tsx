a324d3942e9f20662278d59c804a78c3ce29cd99
ykob <info@tplh.net>
2022-11-17 00:11:47 +0900

    update props for MaterialContent.

--- src/components/features/material-content/MaterialContent.tsx
+++ src/components/features/material-content/MaterialContent.tsx
@@ -4,11 +4,12 @@ type Props = {
   name: string
   effectIds: number[]
   category: number
+  className: string
 }
 
 export const MaterialContent = function (props: Props) {
   return (
-    <div>
+    <div className={props.className}>
       <div className="mb-4 text-4xl">{props.name}</div>
       <CategoryBadge categoryId={props.category} />
     </div>
