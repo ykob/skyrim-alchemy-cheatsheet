50dc83c5c1bba45a82a1a5fbadd6020803737fc0
ykob <info@tplh.net>
2022-11-22 23:42:43 +0900

    adjusted some margins.

--- src/components/features/material-content/MaterialContent.tsx
+++ src/components/features/material-content/MaterialContent.tsx
@@ -9,7 +9,7 @@ type Props = {
 export const MaterialContent = function (props: Props) {
   return (
     <div className={props.className}>
-      <div className="mb-4 text-2xl md:text-3xl lg:text-4xl">{props.name}</div>
+      <div className="mb-2 text-2xl md:mb-4 md:text-3xl lg:text-4xl">{props.name}</div>
       <CategoryBadge categoryId={props.category} />
     </div>
   )
