e94c65ec5460ab4b074de92a4166ad8725a72789
ykob <info@tplh.net>
2022-11-22 23:39:02 +0900

    adjusted text size on the global header.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="pt-8 pb-16 bg-slate-100">
       <div className="flex mb-8 px-4 lg:px-8">
-        <h1 className="text-2xl">Skyrim Archemy Cheatsheet</h1>
+        <h1 className="text-lg lg:text-2xl">Skyrim Archemy Cheatsheet</h1>
       </div>
       <Outlet />
     </div>
