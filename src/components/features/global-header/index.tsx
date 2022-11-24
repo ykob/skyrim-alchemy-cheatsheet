81549e541d18b1deffca1a5fc19d2d5380e8870e
ykob <info@tplh.net>
2022-11-24 23:49:32 +0900

    added the back button on GlobalHeader.

--- src/components/features/global-header/index.tsx
+++ src/components/features/global-header/index.tsx
@@ -1,7 +1,15 @@
-export const GlobalHeader = function () {
+import { memo } from 'react'
+import { Link } from 'react-router-dom'
+
+export const GlobalHeader = memo(function () {
   return (
-    <div className="flex mb-8 px-4 lg:px-8">
+    <div className="flex justify-between mb-8 px-4 lg:px-8">
       <h1 className="md:text-lg lg:text-xl">Skyrim Archemy Cheatsheet</h1>
+      <Link to="/">
+        <button>
+          Back
+        </button>
+      </Link>
     </div>
   )
-}
+})
