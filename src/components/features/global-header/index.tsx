3907c52f54258843c812405cb28296e088b1317d
ykob <info@tplh.net>
2022-11-25 23:34:53 +0900

    split backbutton component on global header.

--- src/components/features/global-header/index.tsx
+++ src/components/features/global-header/index.tsx
@@ -1,15 +1,10 @@
-import { memo } from 'react'
-import { Link } from 'react-router-dom'
+import { BackButton } from "./BackButton"
 
-export const GlobalHeader = memo(function () {
+export const GlobalHeader = function () {
   return (
     <div className="flex justify-between mb-8 px-4 lg:px-8">
       <h1 className="md:text-lg lg:text-xl">Skyrim Archemy Cheatsheet</h1>
-      <Link to="/">
-        <button>
-          Back
-        </button>
-      </Link>
+      <BackButton />
     </div>
   )
-})
+}
