8f50d2842961c3357af448aa43bdb137dcc577cb
ykob <info@tplh.net>
2022-11-26 14:21:08 +0900

    added Home icon in BackButton.

--- src/components/features/global-header/BackButton.tsx
+++ src/components/features/global-header/BackButton.tsx
@@ -1,4 +1,6 @@
 import { Link, useLocation } from 'react-router-dom'
+import Icon from '@mdi/react'
+import { mdiHome } from '@mdi/js'
 import { BoxButton } from '../../ui-parts'
 
 export const BackButton = function () {
@@ -7,7 +9,9 @@ export const BackButton = function () {
   if (location.pathname !== '/') {
     return (
       <Link to="/">
-        <BoxButton className='w-12 h-12 grid place-content-center' circle>Back</BoxButton>
+        <BoxButton className="w-12 h-12 grid place-content-center" circle>
+          <Icon path={mdiHome} size={1.2} />
+        </BoxButton>
       </Link>
     )
   } else {
