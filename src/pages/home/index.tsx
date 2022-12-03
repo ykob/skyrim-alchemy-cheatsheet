178b1cfac7876de14fe5b1166cdb170352863e02
ykob <info@tplh.net>
2022-12-03 18:42:29 +0900

    added a description for home page.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,6 +3,10 @@ import { MaterialList } from '../../components/features/material-list'
 
 export const PageHome = function() {
   return <div className='px-4 lg:px-8'>
+    <p className='mb-8 font-normal'>
+      The Elder Scrolls V: Skyrim® の錬金術に使用する素材と効果の一覧表です。<br />
+      素材名や効果名をタップすることで一覧をフィルタリングできます。
+    </p>
     <MaterialList materials={materials} />
   </div>
 }
