01d2e07a8fcfd4f60b2d2911a53746b538987843
ykob <info@tplh.net>
2022-12-04 17:55:15 +0900

    added the way to switch a base path on vite.config.

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base:
+    process.env.NODE_ENV === 'production'
+      ? '/skyrim-alchemy-cheatsheet/'
+      : './',
+  plugins: [react()],
 })
