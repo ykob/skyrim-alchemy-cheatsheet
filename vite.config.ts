677334a9eb4ad2c6e54cbf4b346dc9f12e3b6eaf
ykob <info@tplh.net>
2022-12-06 23:43:45 +0900

    adjusted base path on vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:
-    process.env.NODE_ENV === 'production'
-      ? '/skyrim-alchemy-cheatsheet/'
-      : './',
+  base: '/skyrim-alchemy-cheatsheet/',
   plugins: [react()],
 })
